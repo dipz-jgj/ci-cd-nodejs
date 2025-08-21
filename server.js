cdb8405ed75fdf3b92b91c18791296a074cf29b9
Dipta AR <adhipta.ar@gmail.com>
2025-08-21 15:35:02 +0700

    Bug Fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const {greeting} = require('./user')
 app.get('/', (req,res) => res.send('This App is running properly!'))
 app.get('/ping', (req,res) => res.send('Pong!'))
 app.get('/youtube', (req,res) => res.send('Hello, youtube indonesia!'))
-app.get('/instagram', (req,res) => res.send('From instagram!'))
+app.get('/instagram', (req,res) => res.send('From Instagram!'))
 app.get('/hello/:name', (req,res) => {
     res.json({message:greeting(req.params.name)})
 })
