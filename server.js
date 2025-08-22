301f1018b73c1876967965a440fbd7b15f98cb50
Dipta AR <adhipta.ar@gmail.com>
2025-08-22 22:59:41 +0700

    Add tiktok endpoint

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.get('/', (req,res) => res.send('This App is running properly!'))
 app.get('/ping', (req,res) => res.send('Pong!'))
 app.get('/youtube', (req,res) => res.send('Hello, youtube indonesia!'))
 app.get('/instagram', (req,res) => res.send('From Instagram!'))
+app.get('/tiktok', (req,res) => res.send('TikTok App!'))
 app.get('/hello/:name', (req,res) => {
     res.json({message:greeting(req.params.name)})
 })
